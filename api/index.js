6b8117533dac5200cbaada5921ae23c530123724
vijay-iptv <vijayan.mca31@gmail.com>
2026-02-14 13:08:08 +0530

    Updated API code new changessddddsssddsdd

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 
 export default async function handler(req, res) {
-
+  try {
     const userToken = "Tsn40dpLWSdLrEp5Tu6vAotKzgL717UZ";
     const subscriberId = "1464687407";
 
@@ -28,20 +28,20 @@ export default async function handler(req, res) {
 
     const responseData = await response.json();
 
+    console.log(responseData)
     if (!responseData?.data?.dashWidewinePlayUrl) {
       return res.status(404).json({ error: "Encrypted URL not found." });
     }
 
     // 2️⃣ Decrypt URL
     const encryptedUrl = responseData.data.dashWidewinePlayUrl;
-       console.log(encryptedUrl)
+     console.log(encryptedUrl)
     let decryptedUrl = decryptUrl(encryptedUrl, secretKey);
-
     console.log(decryptedUrl)
 
     // Replace if needed
     decryptedUrl = decryptedUrl.replace("bpaicatchupta", "bpaita");
-
+        console.log(decryptedUrl)
     // 3️⃣ Fetch redirect manually
     const headResponse = await fetch(decryptedUrl, {
       method: "GET",
@@ -50,6 +50,7 @@ export default async function handler(req, res) {
         "User-Agent": "Mozilla/5.0",
       },
     });
+    console.log(headResponse)
 
     const location = headResponse.headers.get("location");
 
@@ -64,6 +65,10 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ mpdurl });
 
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
+  }
 }
 
 /**
