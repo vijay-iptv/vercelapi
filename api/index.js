579e40addec09a58acd7729c00ea45e39089bcc2
vijay-iptv <vijayan.mca31@gmail.com>
2026-02-14 12:25:18 +0530

    Updated API code new

--- api/index.js
+++ api/index.js
@@ -1,20 +1,17 @@
 export default async function handler(req, res) {
 
-    const userToken = '1464687407';
-    const subscriberId = 'Tsn40dpLWSdLrEp5Tu6vAotKzgL717UZ';
+  $subscriberId = '1464687407';
+  $userToken = 'Tsn40dpLWSdLrEp5Tu6vAotKzgL717UZ';
+  const response = await fetch("https://tb.tapi.videoready.tv/content-detail/api/partner/cdn/player/details/chotiluli/521", {
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + userToken,
+      "subscriberId": subscriberId,
+      "Content-Type": "application/json"
+    }
+  });
 
-    const content_api = "https://tb.tapi.videoready.tv/content-detail/api/partner/cdn/player/details/chotiluli/647";
-    const response = await fetch(content_api, {
-        method: "GET",
-        headers: {
-        "Authorization": "Bearer " + userToken,
-        "subscriberId": subscriberId,
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-        }
-    });
+  const data = await response.json();
 
-    const data = await response.json();
-
-    res.status(200).json(data);
+  res.status(200).json(data);
 }
