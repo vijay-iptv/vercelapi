40ad48129905e24691f489d85cb02c2b752f75a8
vijay-iptv <vijayan.mca31@gmail.com>
2026-02-14 12:28:54 +0530

    Updated API code newd

--- api/index.js
+++ api/index.js
@@ -1,17 +1,29 @@
 export default async function handler(req, res) {
+  try {
 
-  $subscriberId = '1464687407';
-  $userToken = 'Tsn40dpLWSdLrEp5Tu6vAotKzgL717UZ';
-  const response = await fetch("https://tb.tapi.videoready.tv/content-detail/api/partner/cdn/player/details/chotiluli/521", {
-    method: "GET",
-    headers: {
-      "Authorization": "Bearer " + userToken,
-      "subscriberId": subscriberId,
-      "Content-Type": "application/json"
+    const userToken = 'Tsn40dpLWSdLrEp5Tu6vAotKzgL717UZ';
+    const subscriberId = '1464687407';
+
+    const content_api = "https://tb.tapi.videoready.tv/content-detail/api/partner/cdn/player/details/chotiluli/647";
+      const response = await fetch(content_api, {
+        method: "GET",
+        headers: {
+          "Authorization": "Bearer " + userToken,
+          "subscriberId": subscriberId,
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+    });
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: "External API failed" });
     }
-  });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  res.status(200).json(data);
+    return res.status(200).json(data);
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 }
